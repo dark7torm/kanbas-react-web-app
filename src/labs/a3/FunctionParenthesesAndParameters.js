5feac050b5e26bdc437b34aefa54de517f0c973b
Ren Taguchi <132172062+dark7torm@users.noreply.github.com>
2023-10-19 18:48:19 -0400

    lab completed

---
+++ src/labs/a3/FunctionParenthesesAndParameters.js
@@ -0,0 +1,16 @@
+function FunctionParenthesesAndParameters() {
+    const square = a => a * a;
+    const plusOne = a => a + 1;
+    const twoSquared = square(2);
+    const threePlusOne = plusOne(3);
+    return (
+        <>
+        <h3>Parentheses and Parameters</h3>
+        twoSquared = {twoSquared} <br/>
+        square(2) = {square(2)} <br/>
+        threePlusOne = {threePlusOne} <br/>
+        plusOne = {plusOne(3)} <br/>
+        </>
+    )
+}
+export default FunctionParenthesesAndParameters
---
+++ src/labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,18 @@
+import ES5Functions from "./ES5Functions"
+import ArrowFunctions from "./ArrowFunctions"
+import ImpliedReturns from "./ImpliedReturns"
+import FunctionParenthesesAndParameters from "./FunctionParenthesesAndParameters"
+import WorkingWithArrays from "./WorkingWithArrays"
+function WorkingWithFunctions(){
+    return(
+        <div>
+            <ES5Functions/>
+            <ArrowFunctions/>
+            <ImpliedReturns/>
+            <FunctionParenthesesAndParameters/>
+        </div>
+        
+    )
+    
+}
+export default WorkingWithFunctions
